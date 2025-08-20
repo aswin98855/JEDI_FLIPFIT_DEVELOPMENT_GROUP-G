095bd61d3a7e163688e2b7dc54de6ccf02bfb2f2
githubAnjaliOjha <anjaliojha2k21@gmail.com>
2025-08-20 10:28:06 +0530

    feat: Add Booking and Slots bean classes
    # Conflicts:
    #	JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Slot.java

--- JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Booking.java
+++ JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Booking.java
@@ -1,4 +1,83 @@
 package com.flipfit.bean;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Booking {
+    private int bookingId;
+    private int customerId;
+    private int gymId;
+    private int slotId;
+    private String bookingStatus;
+    private LocalDate bookingDate;
+    private LocalTime bookingTime;
+
+    public Booking() {
+    }
+
+    public Booking(int bookingId, int customerId, int gymId, int slotId, String bookingStatus, LocalDate bookingDate, LocalTime bookingTime) {
+        this.bookingId = bookingId;
+        this.customerId = customerId;
+        this.gymId = gymId;
+        this.slotId = slotId;
+        this.bookingStatus = bookingStatus;
+        this.bookingDate = bookingDate;
+        this.bookingTime = bookingTime;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getGymId() {
+        return gymId;
+    }
+
+    public void setGymId(int gymId) {
+        this.gymId = gymId;
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
+
+    public LocalDate getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(LocalDate bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public LocalTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(LocalTime bookingTime) {
+        this.bookingTime = bookingTime;
+    }
 }
--- JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Slot.java
+++ JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Slot.java
@@ -1,4 +1,72 @@
 package com.flipfit.bean;
 
-public class Slot {
+import java.time.LocalTime;
+
+public class Slots {
+    private int slotId;
+    private int gymId;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private int capacity;
+    private int bookedCount;
+
+    public Slots() {
+    }
+
+    public Slots(int slotId, int gymId, LocalTime startTime, LocalTime endTime, int capacity, int bookedCount) {
+        this.slotId = slotId;
+        this.gymId = gymId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.capacity = capacity;
+        this.bookedCount = bookedCount;
+    }
+
+    public int getSlotId() {
+        return slotId;
+    }
+
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+
+    public int getGymId() {
+        return gymId;
+    }
+
+    public void setGymId(int gymId) {
+        this.gymId = gymId;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getBookedCount() {
+        return bookedCount;
+    }
+
+    public void setBookedCount(int bookedCount) {
+        this.bookedCount = bookedCount;
+    }
 }
