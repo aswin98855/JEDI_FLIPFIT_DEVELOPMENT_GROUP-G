2c36dad24101a227df61360a837eb98584a92a7d
Mamta Kanwar <k.mamta2k4@gmail.com>
2025-08-20 10:32:48 +0530

    GymCentre-update

--- JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/GymCentre.java
+++ JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/GymCentre.java
@@ -1,4 +1,89 @@
 package com.flipfit.bean;
-
+import java.util.ArrayList;
 public class GymCentre {
+
+    private int centreId;
+    private int ownerId;
+    private String name;
+    private ArrayList slots;
+    private int capacity;
+    private boolean approved;
+    private String city;
+    private String state;
+    private String pincode;
+
+    public GymCentre() {
+    }
+
+    public int getCentreId() {
+        return centreId;
+    }
+
+    public void setCentreId(int centreId) {
+        this.centreId = centreId;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList getSlots() {
+        return slots;
+    }
+
+    public void setSlots(ArrayList slots) {
+        this.slots = slots;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
 }
