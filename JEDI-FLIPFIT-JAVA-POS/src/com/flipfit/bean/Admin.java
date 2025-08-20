244eac0b6b4a487eace752558cf9f6e70986a727
Aman <agrawal180601@gmail.com>
2025-08-20 10:55:10 +0530

    changes in admin and customer bean

--- JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Admin.java
+++ JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Admin.java
@@ -1,4 +1,32 @@
 package com.flipfit.bean;
 
-public class Admin extends User {
+public class Admin extends Role {
+
+    private int adminId;
+    private String emailId;
+    private String password;
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Customer.java
+++ JEDI-FLIPFIT-JAVA-POS/src/com/flipfit/bean/Customer.java
@@ -1,5 +1,22 @@
 package com.flipfit.bean;
 
 public class Customer extends User {
-    
+    private int paymentType;
+    private String paymentInfo;
+
+    public int getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(int paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public String getPaymentInfo() {
+        return paymentInfo;
+    }
+
+    public void setPaymentInfo(String paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
 }
